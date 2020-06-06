3950c7081f38acb0314e873e8ee7d3e7fc45b60d
Tomas Blondeel <tomasblondeel@gmail.com>
2020-06-06 21:56:17 +0200

    Initial commit

---
+++ src/main/java/com/blondul/investmentval/InvestmentvalApplication.java
@@ -0,0 +1,13 @@
+package com.blondul.investmentval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentvalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentvalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blondul/investmentval/InvestmentvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blondul.investmentval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentvalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
